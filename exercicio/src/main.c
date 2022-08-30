1f6485385f66e6936cdf0859e7a580b7dd3e8132
ravilon <ravilon.ads@inf.ufpel.edu.br>
2022-08-30 06:25:34 -0300

    alterações de organização

--- exercicio/src/main.c
+++ exercicio/src/main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-	void *PVteste;
+	printf("Hello World!\n");
 
-	PVteste = (char*)malloc(5*(sizeof(char)));
-	PVteste = "ravi";
-	printf("%s", (char*) PVteste);
 	return (0);
 }

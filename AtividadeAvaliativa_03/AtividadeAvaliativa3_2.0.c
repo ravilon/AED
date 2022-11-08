b7016e6c74a1bea853dfecbf10da8fa83fc09094
Ravilon <ravilon.ads@inf.ufpel.edu.br>
2022-11-07 22:28:33 -0300

    Alterações no código

--- AtividadeAvaliativa_03/AtividadeAvaliativa3_2.0.c
+++ AtividadeAvaliativa_03/AtividadeAvaliativa3_2.0.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
+/*
+====================
+struct Node
+
+Estrutura de dados para armazenar um nó da árvore
+====================
+*/
 struct Node {
     int number;
     struct Node *left;
     struct Node *right;
 }; typedef struct Node Node;
 
+/*
+====================
+CompareNumbers  
+
+Comapara dois inteiros e retorna 1 se o primeiro for maior, 2 se o segundo for maior e 0 se forem iguais.
+====================
+*/
 int CompareNumbers(int number1, int number2) {
     if (number1 > number2) { return 1; }
     else if (number1 < number2) { return -1; }
     else { return 0; }
 }
 
-Node *newNode(int number) {
+/*
+====================
+newNode 
+
+Cria um novo nó e retorna o endereço de memória do mesmo.
+====================
+*/
+Node *NewNode(int number) {
     Node *temp = (Node*) malloc(sizeof(Node));
     temp->number = number;
     temp->left = NULL;
@@ -21,8 +42,15 @@ Node *newNode(int number) {
     return temp;
 }
 
+/*
+====================
+InsertNode
+
+Insere um novo numero na árvore.
+====================
+*/
 Node *InsertNode( Node *pRoot, int number ) {
-    if ( pRoot == NULL ) { pRoot = newNode( number ); }
+    if ( pRoot == NULL ) { pRoot = NewNode( number ); }
     else {
         if ( CompareNumbers( number, pRoot->number ) == 1) {
             pRoot->right = InsertNode ( pRoot->right, number );
@@ -34,13 +62,20 @@ Node *InsertNode( Node *pRoot, int number ) {
     return pRoot;
 }
 
-int heightTree( Node *pRoot ) {
+/*
+====================
+HeightTree
+
+retorna a altura da árvore
+====================
+*/
+int HeightTree( Node *pRoot ) {
     if (pRoot == NULL) {
         return 0;
     }
     else {
-        int heightLeft = heightTree( pRoot->left );
-        int heightRight = heightTree( pRoot->right );
+        int heightLeft = HeightTree( pRoot->left );
+        int heightRight = HeightTree( pRoot->right );
         if (heightLeft > heightRight) {
             return heightLeft + 1;
         }
@@ -50,6 +85,13 @@ int heightTree( Node *pRoot ) {
     }
 }
 
+/*
+====================
+isBalanced
+
+testa se a árvore está balanceada
+====================
+*/
 void isBalanced ( Node *pRoot ){
     if ( pRoot == NULL )
     {
@@ -70,6 +112,13 @@ void isBalanced ( Node *pRoot ){
     }
 }
 
+/*
+====================
+freeTree    
+
+Limpa a árvore usando pós ordem.
+====================
+*/
 void freeTree( Node *pRoot ){
     if ( pRoot != NULL ) {
         freeTree( pRoot->left );
@@ -79,7 +128,7 @@ void freeTree( Node *pRoot ){
 }
 
 int main () {
-    printf( "===============Arvore de Busca================== \n" );
+    printf( "===============Arvore de Busca================= \n" );
     Node *pRoot = NULL;
     int opt = 0;
     do
